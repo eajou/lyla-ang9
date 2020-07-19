75c08485b7ff7a7ad3ef8104c0bee52febf19910
Krystal <krystal@Krystals-MacBook-Air.local>
2020-07-19 11:11:34 -0400

    Updating gulp for watch builds and OOSCSS

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    watch = require('gulp-watch');
+
+// SASS functions
+gulp.task('sass', function () {
+    return (
+        gulp.src('ux/assets/sass/styles.scss')
+ 
+        // Use sass with the files found, and log any errors
+        .pipe(sass())
+        .on("error", sass.logError)
+
+        .pipe(gulp.dest("ux/assets/css"))
+    );
+});
+
+// Clean the CSS
+gulp.task('cleanCSS', function () {
+    return gulp.src('ux/assets/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('src'));    
+});
+
+// Watch the files
+// gulp.task('watch', gulp.series (
+//     // gulp.parallel(sass),
+//     // watch,
+//     watch('ux/assets/sass/**/*.scss'),  
+//     function(done) {
+//         console.log("Here"); done() 
+//     }
+// ));
+
+gulp.task('watchTask', function() {    
+    watch('ux/assets/sass/**/*.scss', 
+        gulp.series(['build:ux']));
+});
+    
+
+/*
+==============
+ BUILD UX
+==============
+*/
+
+// gulp.task('build:ux', ['sass', 'cleanCSS']);
+
+
+gulp.task('build:ux', gulp.series('sass', 'cleanCSS'));
